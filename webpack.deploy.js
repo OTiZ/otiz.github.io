13103f2e3a75b75506dd6cdee0d6210bd57c902c
Otis J White <otizzile@gmail.com>
2017-09-21 13:03:16 -0500

    re-added AWS

--- webpack.deploy.js
+++ webpack.deploy.js
@@ -37,48 +37,6 @@ function getConfig(config) {
     return Object.assign({}, config, {
         plugins: extendedPlugins
     });
-function getConfig() {
-    return {
-        devtool: 'source-map',
-
-        entry: [
-            path.join(__dirname, 'app/index.js')
-        ],
-
-        output: {
-            path: path.join(__dirname, './app/public'),
-            filename: 'bundle.js'
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    include: path.join(__dirname, 'app'),
-                    loaders: ['babel-loader']
-                }
-            ]
-        },
-
-        plugins: [
-            new webpack.LoaderOptionsPlugin({
-                minimize: true
-            }),
-            new webpack.optimize.UglifyJsPlugin({
-                compress: {
-                    unused: true,
-                    dead_code: true,
-                    warnings: true,
-                    screw_ie8: true
-                },
-                compressor: {
-                    warnings: false
-                },
-                minimize: true,
-                sourceMap: true
-            })
-        ]
-    };
 }
 
 module.exports = getConfig(webpackConfig);
