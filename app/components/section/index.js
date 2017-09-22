d5eaa66f1f7b4e2d07134be842605647c458f966
Otis J White <otizzile@gmail.com>
2017-09-21 23:35:34 -0500

    Update index.js
    added volunteer and awards options

--- app/components/section/index.js
+++ app/components/section/index.js
@@ -5,7 +5,9 @@ const React = require('react');
 const ResumePropTypes = require('../../prop_types/resume');
 const About = require('./about');
 const Work = require('./work');
+const Volunteer = require('./volunteer');
 const Education = require('./education');
+const Awards = require('./awards');
 const Skills = require('./skills');
 const Portfolio = require('./portfolio');
 const References = require('./references');
@@ -15,7 +17,9 @@ const Section = React.createClass({
     propTypes: {
         basics: ResumePropTypes.basics,
         work: ResumePropTypes.workSet,
+        volunteer: ResumePropTypes.volunteerSet,
         education: ResumePropTypes.educationSet,
+        awards: ResumePropTypes.awardsSet,
         skills: ResumePropTypes.skillsSet,
         languages: ResumePropTypes.languagesSet,
         portfolio: ResumePropTypes.publicationsSet,
@@ -32,7 +36,9 @@ const Section = React.createClass({
             <div>
                 <About content={this.props.basics}/>
                 <Work content={this.props.work}/>
+                <Volunteer content={this.props.volunteer}/>
                 <Education content={this.props.education}/>
+                <Awards content={this.props.awards}/>
                 <Skills content={skillsContent}/>
                 <Portfolio content={this.props.portfolio}/>
                 <References content={this.props.references}/>
