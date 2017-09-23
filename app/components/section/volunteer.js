1a0fd8f7110402a6f498952627536702d5ea507e
Otis J White <otizzile@gmail.com>
2017-09-22 23:38:18 -0500

    Update volunteer.js
    fix format errors

--- app/components/section/volunteer.js
+++ app/components/section/volunteer.js
@@ -24,10 +24,8 @@ const Entry = React.createClass({
             <div className='row item'>
                 <div className='twelve columns'>
                     <h3>
-                        <a href={this.props.entry.website}target='_blank'>
-              {this.props.entry.organization}
-        </a>
-        </h3>
+                        <a href={this.props.entry.website}target='_blank'>{this.props.entry.organization}</a>
+                    </h3>
                     <p className='info'>
                         {this.props.entry.position}
                         <span> &bull; </span>
